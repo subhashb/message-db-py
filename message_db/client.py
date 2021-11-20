e9f252c70c03303b87a3e237a61f8fc9e7fe1615
Subhash Bhushan <subhash.bhushan@gmail.com>
2021-11-20 08:18:49 -0800

    Implement read functionality
    Include support for:
    - Paginated message reads
    - Validation for expected version
    - Stream and Category reads
    - Stream Last Message read

--- message_db/client.py
+++ message_db/client.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Dict
 from uuid import uuid4
 
+from psycopg2.extensions import connection
 from psycopg2.extras import Json, RealDictCursor
 
 from message_db.connection import ConnectionPool
@@ -45,6 +46,38 @@ class MessageDB:
             )
         self.connection_pool = connection_pool
 
+    def _write(
+        self,
+        connection: connection,
+        stream_name: str,
+        message_type: str,
+        data: Dict,
+        metadata: Dict = None,
+        expected_version: int = None,
+    ):
+        try:
+            with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+                cursor.execute(
+                    (
+                        "SELECT message_store.write_message(%(identifier)s, %(stream_name)s, %(type)s, "
+                        "%(data)s, %(metadata)s, %(expected_version)s);"
+                    ),
+                    {
+                        "identifier": str(uuid4()),
+                        "stream_name": stream_name,
+                        "type": message_type,
+                        "data": Json(data),
+                        "metadata": Json(metadata) if metadata else None,
+                        "expected_version": expected_version,
+                    },
+                )
+
+                result = cursor.fetchone()
+        except Exception as exc:
+            raise ValueError(exc.args[0].splitlines()[0]) from exc
+
+        return result["write_message"]
+
     def write(
         self,
         stream_name: str,
@@ -54,25 +87,74 @@ class MessageDB:
         expected_version: int = None,
     ):
         conn = self.connection_pool.get_connection()
+
+        try:
+            with conn:
+                position = self._write(
+                    conn, stream_name, message_type, data, metadata, expected_version
+                )
+        finally:
+            self.connection_pool.release(conn)
+
+        return position
+
+    def write_batch(self, stream_name, data, expected_version: int = None):
+        conn = self.connection_pool.get_connection()
+
+        try:
+            with conn:
+                for record in data:
+                    expected_version = self._write(
+                        conn,
+                        stream_name,
+                        record[0],
+                        record[1],
+                        metadata=record[2] if len(record) > 2 else None,
+                        expected_version=expected_version,
+                    )
+        finally:
+            self.connection_pool.release(conn)
+
+        return expected_version
+
+    def read(self, stream_name, position=0, no_of_messages=1000):
+        conn = self.connection_pool.get_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+        if "-" in stream_name:
+            sql = "SELECT * FROM get_stream_messages(%(stream_name)s, %(position)s, %(batch_size)s);"
+        else:
+            sql = "SELECT * FROM get_category_messages(%(stream_name)s, %(position)s, %(batch_size)s);"
+
+        cursor.execute(
+            sql,
+            {
+                "stream_name": stream_name,
+                "position": position,
+                "batch_size": no_of_messages,
+            },
+        )
+        messages = cursor.fetchall()
+
+        conn.commit()
+        cursor.close()
+        self.connection_pool.release(conn)
+
+        return messages
+
+    def read_last_message(self, stream_name):
+        conn = self.connection_pool.get_connection()
         cursor = conn.cursor(cursor_factory=RealDictCursor)
 
         cursor.execute(
-            (
-                "SELECT message_store.write_message(%(identifier)s, %(stream_name)s, %(type)s, "
-                "%(data)s, %(metadata)s, %(expected_version)s);"
-            ),
-            {
-                "identifier": str(uuid4()),
-                "stream_name": stream_name,
-                "type": message_type,
-                "data": Json(data),
-                "metadata": Json(metadata) if metadata else None,
-                "expected_version": expected_version,
-            },
+            "SELECT * from get_last_stream_message(%(stream_name)s);",
+            {"stream_name": stream_name},
         )
+
+        message = cursor.fetchone()
+
         conn.commit()
-
-        result = cursor.fetchone()
-
+        cursor.close()
         self.connection_pool.release(conn)
-        return result["write_message"]
+
+        return message
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,7 @@
+import json
+
+import pytest
+
 from message_db.client import MessageDB
 from message_db.connection import ConnectionPool
 
@@ -22,10 +26,16 @@ class TestClientConstruction:
 
 class TestMessageWrite:
     def test_write_to_store(self, client):
-        pass
+        client.write("testStream-123", "Event1", {"foo": "bar"})
+
+        messages = client.read("testStream-123", 0, 100)
+        assert len(messages) == 1
 
     def test_writing_message_with_metadata(self, client):
-        pass
+        client.write("testStream-123", "Event1", {"foo": "bar"}, {"trace_id": "baz"})
+
+        messages = client.read("testStream-123", 0, 100)
+        assert messages[0]["metadata"] == json.dumps({"trace_id": "baz"})
 
     def test_that_write_returns_position_of_message_written(self, client):
         position = client.write("testStream-123", "Event1", {"foo": "bar"})
@@ -34,31 +44,98 @@ class TestMessageWrite:
         position = client.write("testStream-123", "Event1", {"foo": "bar"})
         assert position == 1
 
-    def test_that_global_position_is_incremented_on_write(self, client):
-        pass
-
     def test_write_message_batch(self, client):
-        pass
+        events = [
+            ("Event1", {"foo1": "bar1"}),
+            ("Event2", {"foo2": "bar2"}),
+            ("Event3", {"foo3": "bar3"}),
+            ("Event4", {"foo4": "bar4"}),
+        ]
+
+        last_position = client.write_batch("testStream-123", events)
+
+        assert last_position == 3
 
     def test_write_with_expected_version(self, client):
-        pass
+        for _ in range(3):
+            last_position = client.write("testStream-123", "Event1", {"foo": "bar"})
+
+        try:
+            # Position would be at 2 by now, so this should succeed
+            client.write(
+                "testStream-123",
+                "Event1",
+                {"foo": "bar"},
+                expected_version=last_position,
+            )
+        except Exception:
+            pytest.fail("Unexpected error with expected version")
 
     def test_that_write_fails_on_expected_version_mismatch(self, client):
-        pass
+        for _ in range(3):
+            client.write("testStream-123", "Event1", {"foo": "bar"})
+        # Position would be at 2 by now, so this should throw an error
+        with pytest.raises(ValueError) as exc:
+            client.write("testStream-123", "Event1", {"foo": "bar"}, expected_version=1)
+
+        assert (
+            exc.value.args[0]
+            == "Wrong expected version: 1 (Stream: testStream-123, Stream Version: 2)"
+        )
 
 
 class TestEventIO:
     def test_read_stream_from_store(self, client):
-        pass
+        client.write("testStream-123", "Event1", {"foo": "bar"})
+
+        messages = client.read("testStream-123", 0, 100)
+        assert messages is not None
+        assert messages[0]["data"] == json.dumps({"foo": "bar"})
 
     def test_read_multiple_stream_messages_from_store(self, client):
-        pass
+        for i in range(5):
+            client.write("testStream-123", "Event1", {f"foo{i}": f"bar{i}"})
+
+        messages = client.read("testStream-123", 0, 100)
+
+        assert messages is not None
+        assert len(messages) == 5
+        assert messages[4]["data"] == json.dumps({"foo4": "bar4"})
+
+    def test_read_paginated_stream_messages_from_store(self, client):
+        for i in range(5):
+            client.write("testStream-123", "Event1", {f"foo{i}": f"bar{i}"})
+
+        messages = client.read("testStream-123", 0, 3)
+
+        assert messages is not None
+        assert len(messages) == 3
+        assert messages[2]["data"] == json.dumps({"foo2": "bar2"})
 
     def test_read_stream_last_message(self, client):
-        pass
+        for i in range(5):
+            client.write("testStream-123", "Event1", {"foo": f"bar{i}"})
+
+        message = client.read_last_message("testStream-123")
+        assert message["position"] == 4
+        assert message["data"] == json.dumps({"foo": "bar4"})
 
     def test_read_specific_stream_message(self, client):
-        pass
+        for i in range(5):
+            client.write("testStream-123", "Event1", {"foo": f"bar{i}"})
+        for i in range(5):
+            client.write("testStream-456", "Event1", {"foo": f"baz{i}"})
+
+        messages = client.read("testStream-456", 0, 100)
+
+        assert len(messages) == 5
+        assert messages[4]["data"] == json.dumps({"foo": "baz4"})
 
     def test_read_category_messages(self, client):
-        pass
+        for i in range(5):
+            client.write("testStream-123", "Event1", {"foo": f"bar{i}"})
+
+        messages = client.read("testStream", 0, 100)
+
+        assert len(messages) == 5
+        assert messages[4]["data"] == json.dumps({"foo": "bar4"})
